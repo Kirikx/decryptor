bbb19f0df786d18bf322cab7ab2cbe230035ac47
Kirill Semenov <kirik@mail.ru>
2022-02-12 16:09:33 +0300

    fix decrypt config

--- src/main/java/ru/devstend/decryptor/config/DecryptConfig.java
+++ src/main/java/ru/devstend/decryptor/config/DecryptConfig.java
@@ -15,46 +15,52 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "decrypt")
 public class DecryptConfig {
 
-    private static final String GOOGLE_VERIFYING_PUBLIC_KEYS_JSON =
-        "{\n"
-            + "  \"keys\": [\n"
-            + "    {\n"
-            + "      \"keyValue\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEPYnHwS8uegWAewQtlxizmLFynw"
-            + "HcxRT1PK07cDA6/C4sXrVI1SzZCUx8U8S0LjMrT6ird/VW7be3Mz6t/srtRQ==\",\n"
-            + "      \"protocolVersion\": \"ECv1\"\n"
-            + "    },\n"
-            + "    {\n"
-            + "      \"keyValue\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE/1+3HBVSbdv+j7NaArdgMyoSAM"
-            + "43yRydzqdg1TxodSzA96Dj4Mc1EiKroxxunavVIvdxGnJeFViTzFvzFRxyCw==\",\n"
-            + "      \"keyExpiration\": \""
-            + Instant.now().plus(Duration.standardDays(1000)).getMillis()
-            + "\",\n"
-            + "      \"protocolVersion\": \"ECv2\"\n"
-            + "    },\n"
-            + "    {\n"
-            + "      \"keyValue\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAENXvYqxD5WayKYhuXQevdGdLA8i"
-            + "fV4LsRS2uKvFo8wwyiwgQHB9DiKzG6T/P1Fu9Bl7zWy/se5Dy4wk1mJoPuxg==\",\n"
-            + "      \"keyExpiration\": \""
-            + Instant.now().plus(Duration.standardDays(1000)).getMillis()
-            + "\",\n"
-            + "      \"protocolVersion\": \"ECv2SigningOnly\"\n"
-            + "    }\n"
-            + "  ]\n"
-            + "}";
+  private static final String GOOGLE_VERIFYING_PUBLIC_KEYS_JSON =
+      "{\n"
+          + "  \"keys\": [\n"
+          + "    {\n"
+          + "      \"keyValue\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEPYnHwS8uegWAewQtlxizmLFynw"
+          + "HcxRT1PK07cDA6/C4sXrVI1SzZCUx8U8S0LjMrT6ird/VW7be3Mz6t/srtRQ==\",\n"
+          + "      \"protocolVersion\": \"ECv1\"\n"
+          + "    },\n"
+          + "    {\n"
+          + "      \"keyValue\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE/1+3HBVSbdv+j7NaArdgMyoSAM"
+          + "43yRydzqdg1TxodSzA96Dj4Mc1EiKroxxunavVIvdxGnJeFViTzFvzFRxyCw==\",\n"
+          + "      \"keyExpiration\": \""
+          + Instant.now().plus(Duration.standardDays(1000)).getMillis()
+          + "\",\n"
+          + "      \"protocolVersion\": \"ECv2\"\n"
+          + "    },\n"
+          + "    {\n"
+          + "      \"keyValue\": \"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAENXvYqxD5WayKYhuXQevdGdLA8i"
+          + "fV4LsRS2uKvFo8wwyiwgQHB9DiKzG6T/P1Fu9Bl7zWy/se5Dy4wk1mJoPuxg==\",\n"
+          + "      \"keyExpiration\": \""
+          + Instant.now().plus(Duration.standardDays(1000)).getMillis()
+          + "\",\n"
+          + "      \"protocolVersion\": \"ECv2SigningOnly\"\n"
+          + "    }\n"
+          + "  ]\n"
+          + "}";
 
-    private String recipientId;
-    private String verifyingPublicKeysJson;
-    private String merchantPrivateKey;
-    private String alternateMerchantPrivateKey;
+  private String recipientId;
+  private String verifyingPublicKeysJson;
+  private String merchantPrivateKey;
+  private String alternateMerchantPrivateKey;
 
-    public void setVerifyingPublicKeysJson(String verifyingPublicKeysJson) {
-//        System.out.println("TEST");
-//        System.out.println(new String(Base64.encodeBase64(GOOGLE_VERIFYING_PUBLIC_KEYS_JSON.getBytes(StandardCharsets.UTF_8))));
-        this.verifyingPublicKeysJson = Optional.ofNullable(verifyingPublicKeysJson)
-            .filter(StringUtils::isNoneBlank)
-            .filter(Base64::isBase64)
-            .map(Base64::decodeBase64)
-            .map(String::new)
-            .orElseThrow(() -> new RuntimeException("VerifyingPublicKeysJson not decode Base64"));
+  public void setVerifyingPublicKeysJson(String verifyingPublicKeysJson) {
+    if (StringUtils.isNoneBlank(verifyingPublicKeysJson)) {
+      this.verifyingPublicKeysJson = Optional.ofNullable(verifyingPublicKeysJson)
+          .filter(StringUtils::isNoneBlank)
+          .filter(Base64::isBase64)
+          .map(Base64::decodeBase64)
+          .map(String::new)
+          .orElseThrow(
+              () -> new RuntimeException("VerifyingPublicKeysJson not decode Base64"));
+    } else {
+      this.verifyingPublicKeysJson = new String(
+          Base64.encodeBase64(
+              GOOGLE_VERIFYING_PUBLIC_KEYS_JSON.getBytes(StandardCharsets.UTF_8)
+          ));
     }
+  }
 }
